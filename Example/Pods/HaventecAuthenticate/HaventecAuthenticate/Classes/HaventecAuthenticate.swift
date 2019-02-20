8e33b7e9e5faf96837ddb46f9d2b7e020aa3d315
Justin Crosbie <justin.crosbie@haventec.com>
2019-02-20 17:27:38 +1100

    Several refactors; Better tests

--- Example/Pods/HaventecAuthenticate/HaventecAuthenticate/Classes/HaventecAuthenticate.swift
+++ Example/Pods/HaventecAuthenticate/HaventecAuthenticate/Classes/HaventecAuthenticate.swift
@@ -59,49 +59,37 @@ public class HaventecAuthenticate {
     /**
      It retrieves the Haventec authKey
      
-     - Throws: `HaventecAuthenticateError.initialiseError`
-     if the initialiseStorage function has not been called.
-     
      - Returns: String Haventec authKey
      */
-    public static func getAuthKey() throws -> String? {
-        return try StorageHelper.getData().authKey;
+    public static func getAuthKey() -> String? {
+        return StorageHelper.getData().authKey;
     }
 
     /**
      It retrieves the Haventec JWT token
      
-     - Throws: `HaventecAuthenticateError.initialiseError`
-     if the initialiseStorage function has not been called.
-     
      - Returns: String Haventec Authenticate JWT token
      */
-    public static func getAccessToken() throws -> String? {
-        return try StorageHelper.getData().accessToken?.token;
+    public static func getAccessToken() -> String? {
+        return StorageHelper.getData().accessToken?.token;
     }
 
     
     /**
      It retrieves the Haventec username
      
-     - Throws: `HaventecAuthenticateError.initialiseError`
-     if the initialiseStorage function has not been called.
-     
      - Returns: String Haventec username
      */
-    public static func getUsername() throws -> String? {
+    public static func getUsername() -> String? {
         return try StorageHelper.getData().username;
     }
     
     /**
      It retrieves the Haventec deviceUuid
      
-     - Throws: `HaventecAuthenticateError.initialiseError`
-     if the initialiseStorage function has not been called.
-     
      - Returns: String Haventec deviceUuid
      */
-    public static func getDeviceUuid() throws -> String? {
-        return try StorageHelper.getData().deviceUuid;
+    public static func getDeviceUuid() -> String? {
+        return StorageHelper.getData().deviceUuid;
     }
 }
